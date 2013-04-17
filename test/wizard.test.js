d1f3275344e7df30365d3c1e1dbc98e3bba7cfc7
Guillermo Peña <guillermo.pena.cardano@gmail.com>
2013-04-17 18:48:05 +0200

    Wizard unit test file added TODO: cleanScreen property? TODO: pre-question property?

--- test/wizard.test.js
+++ test/wizard.test.js
@@ -1,35 +1,28 @@
 /* ___________________________________________________________________________
 
-    Module: step.test.js   Version: v0.1
+    Module: wizard.test.js   Version: v0.1
     Author: Guillermo Peña (guillermo.pena.cardano@gmail.com)
-    Last update: 16 abril 2013 (martes)
+    Last update: 17/04/2013
     
-        Step wizard unit test
+        Wizard test file
 
    ___________________________________________________________________________*/
+
 
 
 // __ Modules _____________________________________________________________
 
 var util = require("util")
 
-
 // __ Main Object _________________________________________________________
 
-var Step = require("../lib/step.js")
+var EasyWizard = require("../lib/easyWizard.js")
+var ew = new EasyWizard()
 
 // __ Common things _______________________________________________________
 
 // __ Test 01 _____________________________________________________________o
 
-var step1 = {	"id": "1"
-			,	"question" : "Your name: "	
-			,	"defaultAnswer": "popo"
-			,	"type": "value"
-			,	"property": "name"
-			,	"nextStep": "2"
-			}
-
-var s = new Step(step1)
-s.run()
-s.emitter.once('next', function() { console.log("Answer: " +  s.getAnswer()) })
+var file1 = "../examples/basic.wizard"
+ew.emitter.once('end', function() { console.log("\nResult:\n" +  util.inspect(ew.getObject())) })
+ew.run(file1)
